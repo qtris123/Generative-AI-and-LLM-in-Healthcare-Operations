bbee8197510f0c0ec3d3b2cbe510dd8d0191e792
Tri Vo <voquangtri2021@gmail.com>
2025-02-24 23:08:38 -0500

    Update faithfulness_calculation.py
    Changes applied to:
    (1 | adjust) _compute_faithfulness_auc:
    include multiple rows in the calculation of faith score and take the average score of them.
    include the calculation for baseline value (when perturb all features)
    (2 | create)  _component_percentage:
    count the frequency of the top features

--- Faithfulness_score/faithfulness_calculation.py
+++ Faithfulness_score/faithfulness_calculation.py
@@ -3,6 +3,8 @@ import numpy as np
 import torch
 import pandas as pd
 import pickle
+from tqdm import tqdm
+from collections import Counter
 
 from explainer import Explainer
 from perturbation_method import NormalPerturbation
@@ -56,6 +58,8 @@ class FaithfulnessScore:
         self.model = explainer.model #model
         self.labels = explainer.labels #labels
         self.test_data = explainer.test_data #test_data
+        self.num_rows = 150 #number of rows taken in the calculation of faith score.
+        self.feature_names = explainer.feature_names_dict #feature names in order of index -> dict{index: name}
 
         self.discrete_features = [2,3,4,5,6,7] # I extract these numbers manually from model.feature_names
 
@@ -77,7 +81,7 @@ class FaithfulnessScore:
                                                       std=self.perturbation_std,
                                                       flip_percentage=self.perturbation_flip_percentage)
     
-
+        self.top_k_features = {}
 
     def _compute_faithfulness_auc(self, metric="topk", k = 3):
         """Computes AUC for faithfulness scores, perturbing top k (where k is an array).
@@ -90,20 +94,29 @@ class FaithfulnessScore:
         """
         faithfulness = 0
         # iterate through the impact from top-1 to top-k feature
-        for k_i in range(1,k+1):
+        indices = self.test_data.sample(n= self.num_rows, random_state=40).index
+        for row_ind in tqdm(indices):
             if self.mode == "LIME":
-                # select explanation values for one instance/row; at the moment, take 9 as an example.
-                exp_instance = self.lime_explainer.explain_instance(np.array(self.test_data[self.test_data.index == 9])[0], self.model.predict_proba, num_features=14, top_labels=k_i)
+                # select explanation values for one instance/row; at the moment, take row indexed 9 as an example.
+                exp_instance = self.lime_explainer.explain_instance(np.array(self.test_data[self.test_data.index == row_ind])[0], self.model.predict_proba, num_features=14, top_labels=k)
                 c_label = list(exp_instance.predict_proba).index(max(exp_instance.predict_proba)) #the most likely class
                 feature_contribution_scores = exp_instance.as_map()[c_label]
+                for k_i in range(1,k+1):
+                    # Construct original mask as all true (i.e., all indices are masked and non are perturbed)
+                    top_k_map = torch.tensor([True] * len(feature_contribution_scores), dtype=torch.bool)
 
-                # Construct original mask as all true (i.e., all indices are masked and non are perturbed)
-                top_k_map = torch.tensor([True] * len(feature_contribution_scores), dtype=torch.bool)
+                    # Unmask topk instances 
+                    top_k_indices = [x[0] for x in exp_instance.as_map()[c_label]][:k_i]
+                    top_k_map[top_k_indices] = False
+                    #self.top_k_features += Counter({self.feature_names[i]: 1 for i, flag in enumerate(top_k_map) if not flag.item()}
 
-                # Unmask topk instances 
-                top_k_indices = [ x[0] for x in exp_instance.as_map()[c_label]][:k_i]
-                top_k_map[top_k_indices] = False
-
+                    # If top-k provide top-k instances 
+                    x = np.array(self.test_data[self.test_data.index == row_ind]) # x input for compute_faithfulness_topk is an np.array with reshape(1,-1) or without [0]
+                    if metric == "topk":
+                        faithfulness += self._compute_faithfulness_topk(x, c_label, top_k_map)
+                    else:
+                        # Otherwise, provide bottom-k indices
+                        faithfulness += self._compute_faithfulness_topk(x, c_label, ~top_k_map)
 
             elif self.mode == "SHAP":
                 if (self.not_trained):
@@ -113,33 +126,48 @@ class FaithfulnessScore:
                 else:
                     with open('shap_explanation.pkl', 'rb') as file:
                         explanation = pickle.load(file)
-                # select explanation values for one instance/row; at the moment, take 9 as an example
+                # select explanation values for one instance/row; at the moment, take row indexed 9 as an example
                 X_test_copy = self.test_data.copy()
                 X_test_copy['index'] = range(X_test_copy.shape[0])
-                ind = X_test_copy[X_test_copy.index == 9]['index'].values[0] #add a (sorted) index column to hep find the index of the row for the explaination values.
+                ind = X_test_copy[X_test_copy.index == row_ind]['index'].values[0] #add a (sorted) index column to help find the index of the row for the explaination values.
                 # the final shap value = base value + sum of the feature contributions (for each class)
                 sum_features = np.sum(explanation[ind,:,:].values, axis = 0) # dim(n_features x n_classes)
                 base_values = self.shap_explainer.expected_value # dim(1 x n_classes) #use shap_explainer not explanation
                 c_label = np.argmax(base_values + sum_features) # the most likely class for an instance
                 feature_contribution_scores = explanation[ind,:,c_label]
 
-                # Construct original mask as all true (i.e., all indices are masked and non are perturbed)
-                top_k_map = torch.tensor([True] * len(feature_contribution_scores), dtype=torch.bool)
+                for k_i in range(1,k+1):
+                    # Construct original mask as all true (i.e., all indices are masked and non are perturbed)
+                    top_k_map = torch.tensor([True] * len(feature_contribution_scores), dtype=torch.bool)
+                    # Unmask topk instances 
+                    top_k_indices = list(np.argsort(abs(feature_contribution_scores.values))[-k_i:])
+                    top_k_map[top_k_indices] = False
 
-                # Unmask topk instances 
-                top_k_indices = list(np.argsort(abs(feature_contribution_scores.values))[-k_i:])
-                top_k_map[top_k_indices] = False
+                    # If top-k provide top-k instances 
+                    x = np.array(self.test_data[self.test_data.index == row_ind]) # x input for compute_faithfulness_topk is an np.array with reshape(1,-1) or without [0]
+                    if metric == "topk":
+                        faithfulness += self._compute_faithfulness_topk(x, c_label, top_k_map)
+                    else:
+                        # Otherwise, provide bottom-k indices
+                        faithfulness += self._compute_faithfulness_topk(x, c_label, ~top_k_map)
 
-            # If top-k provide top-k instances 
-            x = np.array(self.test_data[self.test_data.index == 9]) # x input for compute_faithfulness_topk is an np.array with reshape(1,-1) or without [0]
 
-                
-            if metric == "topk":
-                faithfulness += self._compute_faithfulness_topk(x, c_label, top_k_map)
-            else:
-                # Otherwise, provide bottom-k indices
-                faithfulness += self._compute_faithfulness_topk(x, c_label, ~top_k_map)
-        return faithfulness
+            #count the occurence of feature in top k in this format: {‘feature_name’: {rank_1: 1 (times),rank_ 2: 3 (times),rank_ 3: 10 (times)}, 'feature_name_2': ... }
+            for rank, indx in enumerate(top_k_indices, start=1):
+                feature_name  = self.feature_names[indx]
+                if feature_name not in self.top_k_features:
+                    self.top_k_features[feature_name] = {}
+
+                rank_key = f"rank_{rank}"
+                # Increment the count for this rank or set it to 1 if it doesn't exist yet
+                self.top_k_features[feature_name][rank_key] = self.top_k_features[feature_name].get(rank_key, 0) + 1
+
+            
+
+        #calculate the baseline:
+        all_N_map = torch.tensor([False] * len(feature_contribution_scores), dtype=torch.bool)
+        baseline_score = k*self._compute_faithfulness_topk(x, c_label, all_N_map)
+        return (faithfulness / baseline_score)*100/ self.num_rows
     
 
     def _compute_faithfulness_topk(self, x, label, top_k_mask, num_samples: int = 10_000):
@@ -174,6 +202,28 @@ class FaithfulnessScore:
         if isinstance(x, torch.Tensor):
             return x.detach().cpu().numpy()
         return np.array(x)
+    
+    @staticmethod
+    def _component_percentage(dict) -> dict:
+        """ Returns the percentage of a feature in a specific ranking.
+            dict: the dictionary containing the feature counts
+            rank_no: the rank number to check | format: 'rank_1', 'rank_2', etc.
+            feature: the feature to check | format: 'num_previous_recidivisms', 'age', etc.
+        """
+        component_percentage = {}
+        for feature, value in dict.items():
+            for rank, occurence in dict[feature].items():
+                sum = 0
+                for key, value in dict.items():
+                    sum += dict[key].get(rank, 0)
+                
+                if feature not in component_percentage.keys():
+                    component_percentage[feature] = {rank: dict[feature].get(rank)/sum*100}
+                else:    
+                    component_percentage[feature][rank] = dict[feature].get(rank)/sum*100
+
+        return component_percentage
+
 
     """""
     def explain_instance(self,
@@ -229,3 +279,5 @@ class FaithfulnessScore:
 
 obj = FaithfulnessScore()
 print(f'the faithful score is: {obj._compute_faithfulness_auc()}')
+print(f'top k features: {obj.top_k_features}')
+print(f'component percentages: {obj._component_percentage(obj.top_k_features)}')
